083bffa0c4714c4fb95ea0ec232c0a49f322c363
Mayura Pabasara <mayurapabasara143@gmail.com>
2026-02-05 15:10:36 +0530

    Initial Spring Boot BookingSystem project

---
+++ BookingSystem/src/main/java/mp/BookingSystem/controller/HomeController.java
@@ -0,0 +1,13 @@
+package mp.BookingSystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String getHellow(){
+        return "Hello World";
+    }
+}
